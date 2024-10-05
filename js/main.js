8d4810b7d2fd6519bd47f67d1272b2add3a17c56
RavanIsmayilov <rvanismayilov70@gmail.com>
2024-10-05 23:28:50 +0400

    weather app

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let id = '9505fd1df737e20152fbd78cdb289b6a';
+let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
+let city = document.querySelector('.name');
+let form = document.querySelector("form");
+let temperature = document.querySelector('.temperature');
+let description = document.querySelector('.description');
+let valueSearch = document.getElementById('name');
+let clouds = document.getElementById('clouds');
+let humidity = document.getElementById('humidity');
+let pressure = document.getElementById('pressure');
+let main = document.querySelector('main');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();  
+    if(valueSearch.value != ''){
+        searchWeather();
+    }
+});
+
+const searchWeather = async() => {
+    try {
+        const response = await fetch(url+'&q='+ valueSearch.value)
+        const data = await response.json()
+    
+        if(response.ok){
+            city.querySelector('figcaption').innerHTML = data.name;
+            city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+            temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+            temperature.querySelector('span').innerText = data.main.temp;
+            description.innerText = data.weather[0].description;
+            clouds.innerText = data.clouds.all;
+            humidity.innerText = data.main.humidity;
+            pressure.innerText = data.main.pressure;
+        }else {
+            throw new Error(data.message);
+        }   
+    } catch (error) {
+        main.classList.add('error');
+        setTimeout(() => {
+            main.classList.remove('error');
+        }, 1000);
+        
+    } finally{
+        valueSearch.value = '';
+    }
+}
+
+// search Default
+const initCountry = () => {
+    valueSearch.value = 'London';
+    searchWeather();
+}
+initCountry();
